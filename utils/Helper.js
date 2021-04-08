3495cf2e93a4478e5734f833443a660893841fd4
dblythy <daniel-blyth@live.com.au>
2021-04-08 13:11:48 +1000

    Update Helper.js

--- utils/Helper.js
+++ utils/Helper.js
@@ -110,4 +110,29 @@ export default {
       data.filter = Object.assign([], data.all.filter(filter));
     });
   },
+  $timeSince(timeStamp) {
+    const now = new Date();
+    const secondsPast = (now.getTime() - timeStamp) / 1000;
+    if (secondsPast < 60) {
+      return "just now";
+    }
+    if (secondsPast < 3600) {
+      return `${parseInt(secondsPast / 60)}m`;
+    }
+    if (secondsPast <= 86400) {
+      return `${parseInt(secondsPast / 3600)}h`;
+    }
+    if (secondsPast > 86400) {
+      const day = timeStamp.getDate();
+      const month = timeStamp
+        .toDateString()
+        .match(/ [a-zA-Z]*/)[0]
+        .replace(" ", "");
+      const year =
+        timeStamp.getFullYear() == now.getFullYear()
+          ? ""
+          : ` ${timeStamp.getFullYear()}`;
+      return `${day} ${month}${year}`;
+    }
+  },
 };
