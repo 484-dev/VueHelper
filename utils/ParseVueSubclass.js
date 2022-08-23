51ccb095f5186dc06291eca52613b65442b104e7
dblythy <daniel-blyth@live.com.au>
2022-08-23 20:19:41 +1000

    Update ParseVueSubclass.js

--- utils/ParseVueSubclass.js
+++ utils/ParseVueSubclass.js
@@ -1,9 +1,17 @@
-import { Parse } from 'parse';
-import proxyHandler from './Proxy';
+import { Parse } from "parse";
+import proxyHandler from "./Proxy";
 class ParseVueObject extends Parse.Object {
   constructor(...args) {
     super(...args);
     return new Proxy(this, proxyHandler);
+  }
+  async debounce(delay = 500) {
+    if (this._timeout) {
+      clearTimeout(this._timeout);
+    }
+    this._timeout = setTimeout(() => {
+      this.save();
+    }, delay);
   }
 }
 export default ParseVueObject;
