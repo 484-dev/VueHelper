77af361db1d67ec5371da8dc277438826c1920b7
dblythy <daniel-blyth@live.com.au>
2022-08-29 16:47:32 +1000

    Update Proxy.js

--- utils/Proxy.js
+++ utils/Proxy.js
@@ -1,40 +1,44 @@
-const _internalFields = Object.freeze([
-  'objectId',
-  'id',
-  'className',
-  'attributes',
-  'createdAt',
-  'updatedAt',
-  'then',
-]);
 const proxyHandler = {
   get(target, key, receiver) {
     const value = target[key];
     const reflector = Reflect.get(target, key, receiver);
-    if (
-      typeof value === 'function' ||
-      key.toString().charAt(0) === '_' ||
-      _internalFields.includes(key.toString())
-    ) {
-      return reflector
+    if (proxyHandler._isInternal(key, value)) {
+      return reflector;
     }
     return receiver.get(key) ?? reflector;
   },
 
   set(target, key, value, receiver) {
     const current = target[key];
-    if (
-      typeof current !== 'function' &&
-      !_internalFields.includes(key.toString()) &&
-      key.toString().charAt(0) !== '_'
-    ) {
-      receiver.set(key, value);
+    if (proxyHandler._isInternal(key, current)) {
+      return Reflect.set(target, key, value, receiver);
     }
-    return Reflect.set(target, key, value, receiver);
+    return receiver.set(key, value);
   },
 
-  deleteProperty(target, prop) {
-    return target.unset(prop);
-  }
+  deleteProperty(target, key) {
+    const current = target[key];
+    if (proxyHandler._isInternal(key, current)) {
+      return delete target[key];
+    }
+    return target.unset(key);
+  },
+
+  _isInternal(key, value) {
+    const internalFields = Object.freeze([
+      'objectId',
+      'id',
+      'className',
+      'attributes',
+      'createdAt',
+      'updatedAt',
+      'then',
+    ]);
+    return (
+      typeof value === 'function' ||
+      key.toString().charAt(0) === '_' ||
+      internalFields.includes(key.toString())
+    );
+  },
 };
 export default proxyHandler;
