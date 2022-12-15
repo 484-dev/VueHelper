ab32505e854c382bf2be0de9cba056a063158a70
dblythy <daniel-blyth@live.com.au>
2022-12-15 18:42:41 +1100

    wip

--- utils/Helper.js
+++ utils/Helper.js
@@ -2,7 +2,7 @@ import ParseVueObject from "./ParseVueSubclass";
 import imageCompression from "browser-image-compression";
 import sanitizeHtml from 'sanitize-html';
 import { Platform, copyToClipboard, date } from "quasar";
-import { Parse } from 'parse'
+import Parse from 'parse/dist/parse.min.js';
 const makeId = (length = 8) => {
   let result = "";
   const characters =
--- utils/ParseUserSubclass.js
+++ utils/ParseUserSubclass.js
@@ -1,4 +1,4 @@
-import { Parse } from 'parse'
+import Parse from 'parse/dist/parse.min.js';
 import proxyHandler from './Proxy';
 class ParseUser extends Parse.User {
   constructor(...args) {
